d6dc8da19a028b637f8ad11f0ecc5fc8847493df
Mahmod 3ashry <64699750+mahmoud791@users.noreply.github.com>
2022-05-16 22:18:11 +0200

    Update train.py

--- train.py
+++ train.py
@@ -39,3 +39,35 @@ for image in images:
     
 data_info = data_look(cars, notcars)
 
+#Visualize some input images
+
+
+num_images = 10
+
+# Just for fun choose random car / not-car indices and plot example images   
+cars_samples = random.sample(list(cars), num_images)
+notcar_samples = random.sample(list(notcars), num_images)
+    
+# Read in car / not-car images
+car_images = []
+notcar_images = []
+for sample in cars_samples:
+    car_images.append(mpimg.imread(sample))
+    
+for sample in notcar_samples:
+    notcar_images.append(mpimg.imread(sample))
+
+
+orient = 9
+pix_per_cell = 8
+cell_per_block = 2
+
+print("start hog")
+
+car_features, hog_image = get_hog_features(cv2.cvtColor(car_images[1], cv2.COLOR_RGB2GRAY), orient, pix_per_cell, cell_per_block, 
+                        vis=True, feature_vec=True)
+
+notcar_features, notcar_hog_image = get_hog_features(cv2.cvtColor(notcar_images[2], cv2.COLOR_RGB2GRAY), orient, pix_per_cell, cell_per_block, 
+                        vis=True, feature_vec=True)
+
+print("end hog")
