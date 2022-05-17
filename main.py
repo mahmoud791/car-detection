e876c0b33389ee4064dd2c143125ae302b8c9958
mostafawagih <73798262+mostafawagih@users.noreply.github.com>
2022-05-17 07:42:33 +0200

    Update main.py

--- main.py
+++ main.py
@@ -18,3 +18,23 @@ pix_per_cell = dist_pickle["pix_per_cell"]
 cell_per_block = dist_pickle["cell_per_block"]
 spatial_size = dist_pickle["spatial_size"]
 hist_bins = dist_pickle["hist_bins"]
+
+
+def get_rectangles(image, scales = [1, 1.5, 2, 2.5, 3], 
+                   ystarts = [400, 400, 450, 450, 460], 
+                   ystops = [528, 550, 620, 650, 700]):
+    out_rectangles = []
+    for scale, ystart, ystop in zip(scales, ystarts, ystops):
+        rectangles = find_cars(image, ystart, ystop, scale, svc, X_scaler, orient, pix_per_cell, cell_per_block, spatial_size, hist_bins)
+        if len(rectangles) > 0:
+            out_rectangles.append(rectangles)
+    out_rectangles = [item for sublist in out_rectangles for item in sublist] 
+    return out_rectangles
+
+
+    
+
+result_images = []
+result_boxes = []
+heatmap_images = []
+result_img_all_boxes = []
