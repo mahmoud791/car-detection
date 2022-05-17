f4e8a84fbccd2e7695d4d804c84a5a42fc3aeb4a
El-Tabakh <73995540+El-Tabakh@users.noreply.github.com>
2022-05-17 19:45:04 +0200

    color histogram and bin spatial added

--- utilities.py
+++ utilities.py
@@ -67,3 +67,22 @@ def get_hog_features(img, orient, pix_per_cell, cell_per_block,
                        cells_per_block=(cell_per_block, cell_per_block), transform_sqrt=True, 
                        visualize=vis, feature_vector=feature_vec)
         return features
+
+
+def bin_spatial(img, size=(16, 16)):
+    color1 = cv2.resize(img[:,:,0], size).ravel()
+    color2 = cv2.resize(img[:,:,1], size).ravel()
+    color3 = cv2.resize(img[:,:,2], size).ravel()
+    return np.hstack((color1, color2, color3))
+
+
+
+def color_hist(img, nbins=32, bins_range=(0, 256)):
+    # Compute the histogram of the color channels separately
+    channel1_hist = np.histogram(img[:,:,0], bins=nbins, range=bins_range)
+    channel2_hist = np.histogram(img[:,:,1], bins=nbins, range=bins_range)
+    channel3_hist = np.histogram(img[:,:,2], bins=nbins, range=bins_range)
+    # Concatenate the histograms into a single feature vector
+    hist_features = np.concatenate((channel1_hist[0], channel2_hist[0], channel3_hist[0]))
+    # Return the individual histograms, bin_centers and feature vector
+    return hist_features
